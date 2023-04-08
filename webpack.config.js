3a02598bc98f44b8862330d3e3891cbfed7b523d
root <muhammedsafabicakci@gmail.com>
2023-04-08 10:54:39 +0300

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+});
+module.exports = {
+    entry: "./src/index.js",
+    output: { 
+        path: path.join(__dirname, 'dist'),
+        filename: "[name].js"
+    }, 
+    plugins: [htmlPlugin],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'images/[hash]-[name].[ext]',
+                        },
+                    },
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
+};
